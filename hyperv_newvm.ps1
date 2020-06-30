ef280ff8ecf7c200a0bbd732dd153189b3beaacd
John Masci <john.masci@gmail.com>
2020-06-30 19:45:24 -0400

    Create hyperv_newvm.ps1

---
+++ hyperv_newvm.ps1
@@ -0,0 +1,36 @@
+<#
+usage example: ./hyperv_newvm.ps1 -Name Ansible -cpuCount 4 -memCount 8GB
+will create a VM like Ansible-Ubuntu-8473 with 4 vCPU and 8GB of RAM
+#>
+
+param (
+	[string]$name = "new",
+	[int]$cpuCount = 2,
+	[string]$memCount = 4GB
+)
+
+<# set environment specific locations & network #>
+
+$hypervRootPath = "D:\Hyper-V\" #set the path for the VM and VHDx
+$virtualSwitch = "VirtualSwitch2" #set the virtual switch
+$baseVHD = "E:\Templates\Ubuntu-DockerSSH.vhdx" #set your prepped base VHD with OS
+
+<# script execution #>
+
+$vmSuffix = "-Ubuntu-" + (Get-Random 9999)
+
+$newVMName = $name + $vmSuffix
+
+echo "VM Name is $newVMName with $cpuCount vCPUs & $memCount of RAM"
+
+$memInt = ($memCount / 1) #convert string to Int64
+
+New-VM -Name $newVMName -MemoryStartupBytes $memInt -Path $hypervRootPath -Generation 1 -SwitchName $virtualSwitch #create VM
+
+Set-VMProcessor $newVMName -Count $cpuCount #set vCPU
+
+Copy-Item -Path $baseVHD -Destination $hypervRootPath$newVMName #copy VHDx File
+
+Add-VMHardDiskDrive -VMName $newVMName -Path "D:\Hyper-V\$newVMName\Ubuntu-DockerSSH.vhdx" #add disk
+
+Start-VM $newVMName
